36d18071221680fff384c5378b375f8830cb043f
Svetlana4278Zh <svetlana.zh@inbox.ru>
2023-11-12 00:10:32 +0300

    First commit

--- src/main/java/com/example/socks/SocksApplication.java
+++ src/main/java/com/example/socks/SocksApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SocksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(SocksApplication.class, args);
 	}
 
 }
